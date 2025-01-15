37cbc33b6c0b95b3c2cab5219a8ac79fb6c6b72c
fadhil <fadhilrizqyfirmansyah@gmail.com>
2025-01-15 14:38:40 +0700

    tugas 4

--- tugas/tugas4.php
+++ tugas/tugas4.php
@@ -1,13 +1,9 @@
- <!-- 
-Tugas 4
- Buatlah program untuk menghitung volume dari prisma segitiga dengan menentukan nilai awal
- Contoh :
- a = 5
- b = 3
- t = 10
- volume dari prisma segitiga adalah ... cm 
- -->
-
- <?php
-  //
+<?php 
+function Hitungvolume($a,$b,$c){
+    $alas=0.5*$a*$b;
+    $volume= $alas*$c;
+    return $volume;
+}
+$volume=Hitungvolume(2,4,10);
+echo "volume prisma segitiga adalah $volume cm <br>"
 ?>
