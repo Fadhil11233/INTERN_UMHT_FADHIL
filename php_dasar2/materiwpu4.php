91d93fc74d631b242453fcb15601467cfb2d6de4
fadhil <fadhilrizqyfirmansyah@gmail.com>
2025-01-20 12:44:37 +0700

    baru

---
+++ php_dasar2/materiwpu4.php
@@ -0,0 +1,80 @@
+<?php 
+// sintaks php
+
+// standar output 
+// echo,print 
+// print_r 
+// var_dump
+
+// echo "fadhil R.F";
+// print "Fadhil R.F";
+// print_r("Fadhil R.F");
+// var_dump("Fadhil R.F");
+
+// boolean
+// echo true; 
+// echo false;
+
+// variabel dan tipe data 
+// variabel
+// tidak boleh diawali dengan angka, tapi boleh mengandung angka 
+// $nama = "Fadhil";
+// // operator aritmatika + - * / %
+// $a = 10;
+// $y = 20;
+
+// echo $a + $y;
+// echo $a - $y;
+// echo $a * $y;
+// echo $a / $y;
+// echo $a % $y;
+// penggabungan string / concatenation / concat
+// .
+// $nama_depan = "Fadhil";
+// $nama_belakang = "R.F";
+// echo $nama_depan . " " . $nama_belakang;
+
+// assignment
+// =,+=,-=,*=,/=,%=, .=
+// $x = 1;
+// $x -= 5;
+// echo $x;
+// $nama = "fadhil";
+// $nama .= " ";
+// $nama .= "R.F";
+// echo $nama;
+
+// perbandingan 
+// <, >, <=, >=, ==, !=
+// var_dump(1 == "1");
+
+// identitas
+// ===, !==
+// var_dump(1 === "1");
+
+// logika
+// &&, ||, ! 
+// $x = 10;
+// var_dump($x < 20 || $x % 2 == 0);
+
+?>
+<!-- // penulisa sintaks php 
+// 1 php dalam html 
+// 2 html di dalam php   -->
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>belajar php</title>
+</head>
+<body>
+    <h1>halo, selamat datang <?php echo "Fadhil R.F"; ?> </h1>
+    <h1>halo, selamat datang <?php echo $nama; ?> </h1>
+    <p><?php echo "ini adalah paragraf";?></p>
+
+<?php 
+// echo "<h1>halo, selamat datang Fadhil </h1>";
+// ?>
+</body>
+</html> -->
